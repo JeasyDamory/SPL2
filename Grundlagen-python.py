c87bdc14fc5b664924873bbcfb895d488adfaf76
JeasyDamory <171wita30@o365.htl-leoben.at>
2018-04-23 14:15:52 +0200

    saving progress

---
+++ Grundlagen-python.py
@@ -0,0 +1,17 @@
+# Grundlagen-python.py
+
+# Kommentare erfolgen mit hashtags
+
+# Ausgabe von Daten
+print("Hello World")
+
+# Variablen definieren (kann ohne Typ erfolgen)
+heimat = "Earth"
+
+print(heimat, "to World: ", "Hello!")
+
+# Eingabe
+wer = input("And who are you? ")
+
+# und gibt den Text wieder aus
+print("Hey ", wer, "!")
